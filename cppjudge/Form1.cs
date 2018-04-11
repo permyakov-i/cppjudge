c1d25f890bdd3d8f5daefecfd0c9f79bf7d26390
permyakov-i <kirkeinos@gmail.com>
2018-04-11 22:18:19 +0300

    Добавлена функция для проведения теста.

--- cppjudge/Form1.cs
+++ cppjudge/Form1.cs
@@ -29,6 +29,67 @@ namespace tinycpp
             compileCode();
         }
 
+        public void runTest(string fileName)
+        {
+            string result;
+            // Прочитать файл теста
+            string[] readText = File.ReadAllLines(fileName);
+
+            string exeName = Path.GetFileNameWithoutExtension(CurrentFile);
+            Process proc = new Process();
+            proc.StartInfo.FileName = exeName;
+            //proc.StartInfo.Arguments = CurrentFile + " -o" + exeName;
+            proc.StartInfo.RedirectStandardInput = true;
+            proc.StartInfo.CreateNoWindow = false;
+            proc.StartInfo.RedirectStandardError = true;
+            proc.StartInfo.RedirectStandardOutput = true;
+            proc.StartInfo.UseShellExecute = false;
+            proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            proc.EnableRaisingEvents = true;
+
+            var errors = new StringBuilder();
+            var output = new StringBuilder();
+            var hadErrors = false;
+
+            // Получить вывод
+            proc.OutputDataReceived += (s, d) => {
+                output.Append(d.Data);
+            };
+
+            // Получить ошибки
+            proc.ErrorDataReceived += (s, d) => {
+            if (!hadErrors)
+            {
+                hadErrors = !String.IsNullOrEmpty(d.Data);
+            }
+                errors.Append(d.Data);
+            };
+
+            proc.Start();
+            // Слушать поток
+            proc.BeginErrorReadLine();
+            proc.BeginOutputReadLine();
+            long memoryUsed = proc.PrivateMemorySize64;
+            foreach (string s in readText)
+            {
+                proc.StandardInput.WriteLine(s);
+            }         
+            proc.WaitForExit();
+            // Проверка памяти процесса
+            
+            if (memoryUsed>256)
+            {
+                result = "[FAIL] Out of memory " + memoryUsed.ToString() + "used";
+            }
+            string stdout = output.ToString();
+            string stderr = errors.ToString();
+
+            if (proc.ExitCode != 0 || hadErrors)
+            {
+                MessageBox.Show("error:" + stderr);
+            }
+        }
+
         /* Скомпилировать тестируемую программу
          */
         public void compileCode ()
@@ -97,48 +158,26 @@ namespace tinycpp
 
         public void ProcessDirectory(string targetDirectory)
         {
+            comboBox1.Items.Clear();
             // Process the list of files found in thedirectory.
             string[] fileEntries = Directory.GetFiles(targetDirectory);
             foreach (string fileName in fileEntries)
+            {
                 ProcessFile(fileName);
+                if (fileName.EndsWith(".a")
+                {
+
+                } else
+                {
+                    runTest(fileName);
+                }
+            }
         }
+
 
         public void ProcessFile(string path)
         {
-            comboBox1.Items.Clear();
             comboBox1.Items.Add(path);
         }
-
-        /*
-        void StartProcess(bool hidden, string command, params string[] args)
-        {
-            ProcessStartInfo pStartInfo = new ProcessStartInfo();
-
-            pStartInfo.FileName = command;
-            pStartInfo.Arguments = string.Join(" ", args);
-            pStartInfo.UseShellExecute = false;
-
-            if (hidden)
-            {
-                pStartInfo.RedirectStandardError = true;
-                pStartInfo.RedirectStandardOutput = true;
-                pStartInfo.CreateNoWindow = true;
-            }
-
-            Process proc = new Process();
-            proc.StartInfo = pStartInfo;
-
-            proc.Start();
-
-            textBox2.Clear();
-
-            if (hidden)
-            {
-                while (!proc.StandardError.EndOfStream)
-                {
-                    textBox2.AppendText("  Error: " + proc.StandardError.ReadLine() + Environment.NewLine);
-                }
-            }
-        }*/
     }
 }
