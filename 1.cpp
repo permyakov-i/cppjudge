6d42893b44858d29a204b6ec5398368a9536ff5d
permyakov-i <kirkeinos@gmail.com>
2018-05-11 13:42:32 +0300

    Новый тестовый файл и readme.

---
+++ 1.cpp
@@ -0,0 +1,13 @@
+#include <cstring>
+#include <cstdio>
+
+int main(int argc, char** argv)
+{
+	int a = 0, b = 0;
+	scanf("%d", &a);
+	scanf("%d", &b);
+	printf("%d\n", a+b);
+	system("pause");
+	_getch();
+	return 0;
+}
