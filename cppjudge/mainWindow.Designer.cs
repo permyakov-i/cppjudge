9369b555ab0db1d3ff2e0a038fc2e61498cebc13
permyakov-i <kirkeinos@gmail.com>
2018-05-20 00:20:21 +0300

    Отключена возможность клика в окне, где отображаются файлы в папке с тестами по причине отсуствия необходимости.

--- cppjudge/mainWindow.Designer.cs
+++ cppjudge/mainWindow.Designer.cs
@@ -139,6 +139,7 @@
             this.type,
             this.lastModified});
             this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Enabled = false;
             this.listView1.Location = new System.Drawing.Point(0, 0);
             this.listView1.Name = "listView1";
             this.listView1.Size = new System.Drawing.Size(278, 332);
